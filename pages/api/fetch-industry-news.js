96877adf2ae5050b18500bfc6ae261f6480dc34d
Emil Engdahl <75441191+lime02lime@users.noreply.github.com>
2025-03-03 11:42:55 +0000

    Update fetch-industry-news.js
    change aws region

--- pages/api/fetch-industry-news.js
+++ pages/api/fetch-industry-news.js
@@ -4,7 +4,7 @@ import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb';
 
 // Initialize the DynamoDB client
 const client = new DynamoDBClient({
-  region: process.env.AWS_REGION || 'us-east-1',
+  region: process.env.AWS_REGION || 'eu-west-2',
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
